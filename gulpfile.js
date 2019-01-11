9fc07c14e5781125d27d36bd0e692266075f05d0
aiegoo <eozz21@gmail.com>
2019-01-11 09:52:08 +0900

    added udemy default folder and a css course to the exisitng repo with travel-site-files

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require("gulp");
+gulp.task("default", function(){
+	console.log("hooray - I have created a gulpt task");
+});
+
+gulp.task("html", function() {
+console.log("imagine something useful being done to your HTML here.");
+});
